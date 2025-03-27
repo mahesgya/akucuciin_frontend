e6d836f5992248705fdbf85fed0dcdf3773839b9
unknown <anargya115@gmail.com>
2025-03-27 15:28:35 +0800

    add service worker

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ import { BrowserRouter, useRoutes } from "react-router-dom";
 import { store, persistor } from "./redux/store"; 
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { register as registerServiceWorker } from './serviceWorkerRegistration';
+
+registerServiceWorker();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
---
+++ src/serviceWorkerRegistration.js
@@ -0,0 +1,57 @@
+// serviceWorkerRegistration.js
+
+const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+    window.location.hostname === '[::1]' ||
+    window.location.hostname.match(
+      /^127(?:\.\d+){3}$/
+    )
+  );
+  
+  export function register() {
+    if ('serviceWorker' in navigator) {
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+  
+      if (isLocalhost) {
+        checkValidServiceWorker(swUrl);
+      } else {
+        registerValidSW(swUrl);
+      }
+    }
+  }
+  
+  function registerValidSW(swUrl) {
+    navigator.serviceWorker
+      .register(swUrl)
+      .then(registration => {
+        console.log('Service Worker registered:', registration);
+      })
+      .catch(error => {
+        console.error('Service Worker registration failed:', error);
+      });
+  }
+  
+  function checkValidServiceWorker(swUrl) {
+    fetch(swUrl)
+      .then(response => {
+        if (response.status === 404) {
+          navigator.serviceWorker.ready.then(registration => {
+            registration.unregister();
+          });
+        } else {
+          registerValidSW(swUrl);
+        }
+      })
+      .catch(() => {
+        console.log('No internet connection. Running offline mode.');
+      });
+  }
+  
+  export function unregister() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.ready.then(registration => {
+        registration.unregister();
+      });
+    }
+  }
+  
