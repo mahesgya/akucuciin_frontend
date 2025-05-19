08a170c679fe40cb6e48701a995f813768693dc4
unknown <anargya115@gmail.com>
2025-05-19 22:39:44 +0700

    feat: add dispatch at oauth page

--- src/pages/oauth/index.jsx
+++ src/pages/oauth/index.jsx
@@ -1,10 +1,13 @@
 import { useEffect } from "react";
 import { useSearchParams, useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
+import { useDispatch } from "react-redux";
+import { checkAuth, setLogin } from "../../redux/auth.slicer";
 
 const GoogleOauthRedirect = () => {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const storeTokens = async () => {
@@ -23,12 +26,15 @@ const GoogleOauthRedirect = () => {
         Cookies.set("refreshToken", refreshToken, { secure: false, sameSite: "none", expires: 7 });
 
         await new Promise((r) => setTimeout(r, 300));
+        dispatch(setLogin({ accessToken, refreshToken }));
+        await dispatch(checkAuth());
+
         navigate("/");
       }
     };
 
     storeTokens();
-  }, [searchParams, navigate]);
+  }, [searchParams, navigate, dispatch]);
 
   return (
     <div className="flex items-center justify-center h-screen bg-white">
