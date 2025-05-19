099d2f5f8b38d751de3c4d3075a187dcdb29172d
unknown <anargya115@gmail.com>
2025-05-20 00:15:41 +0700

    feat: add transform phone number

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -76,8 +76,26 @@ const Profile = () => {
     }
   };
 
+   const transformPhoneNumber = (phone) => {
+    if (!phone) return "";
+
+    let cleaned = phone.replace(/\D/g, "");
+
+    if (cleaned.startsWith("0")) {
+      cleaned = "62" + cleaned.substring(1);
+    }
+
+    if (!cleaned.startsWith("62")) {
+      cleaned = "62" + cleaned;
+    }
+
+    return cleaned;
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    
+    editProfile.telephone = transformPhoneNumber(editProfile.telephone)
 
     if (accessToken) {
       await customerController.handleProfile(editProfile, accessToken, dispatch, setProfile, setEditProfile, setIsEditing);
