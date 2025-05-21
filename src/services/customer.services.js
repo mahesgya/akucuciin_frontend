a301b709105b8f89a8217fc6d8cc359f3e4e6b1b
unknown <anargya115@gmail.com>
2025-05-21 17:57:12 +0700

    refactor: swal refactor at customer.services

--- src/services/customer.services.js
+++ src/services/customer.services.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import Swal from "sweetalert2";
 import { errorSwal, successSwal } from "../utils/alert.utils";
 
 const customerServices = {
@@ -28,19 +27,12 @@ const customerServices = {
       const response = await axios.post(`${process.env.REACT_APP_BASE_BACKEND_URL}/api/order`, formData, {
         headers: { Authorization: `Bearer ${accessToken}`, "Content-Type": "application/json" },
       });
-      
-      successSwal("Pesanan berhasil dikirim.")
+
+      successSwal("Pesanan berhasil dikirim.");
       navigate("/");
       return response.data;
     } catch (error) {
-      await Swal.fire({
-        icon: "error",
-        title: "Gagal Mengirim Order.",
-        text: error.response?.data?.errors || "Terjadi kesalahan, coba lagi.",
-        confirmButtonText: "Coba Lagi",
-        confirmButtonColor: "#d33",
-        showCloseButton: true,
-      });
+      errorSwal(error.response?.data?.errors);
     } finally {
       setIsLoading(false);
     }
@@ -51,25 +43,10 @@ const customerServices = {
         headers: { Authorization: `Bearer ${accessToken}`, "Content-Type": "application/json" },
       });
 
-      await Swal.fire({
-        icon: "success",
-        title: "Berhasil",
-        text: "Referral Code Anda Berhasil Dibuat.",
-        confirmButtonText: "OK",
-        confirmButtonColor: "#3085d6",
-        showCloseButton: true,
-      });
-
+      successSwal("Berhasil membuat kode referral.");
       return response.data;
     } catch (error) {
-      await Swal.fire({
-        icon: "error",
-        title: "Gagal Mengirim Referral Code.",
-        text: error.response?.data?.errors || "Terjadi kesalahan, coba lagi.",
-        confirmButtonText: "Coba Lagi",
-        confirmButtonColor: "#d33",
-        showCloseButton: true,
-      });
+      errorSwal(error.response?.data?.errors);
     }
   },
   getOrderLaundry: async (accessToken) => {
@@ -79,14 +56,7 @@ const customerServices = {
       });
       return response.data;
     } catch (error) {
-      await Swal.fire({
-        icon: "error",
-        title: "Gagal Mendapatkan Data Order.",
-        text: error.response?.data?.errors || "Terjadi kesalahan, coba lagi.",
-        confirmButtonText: "Coba Lagi",
-        confirmButtonColor: "#d33",
-        showCloseButton: true,
-      });
+      errorSwal(error.response?.data?.errors);
     }
   },
   cancelOrder: async (accessToken, orderId) => {
@@ -95,25 +65,10 @@ const customerServices = {
         headers: { Authorization: `Bearer ${accessToken}`, "Content-Type": "application/json" },
       });
 
-      await Swal.fire({
-        icon: "success",
-        title: "Berhasil",
-        text: "Order Berhasil Di Batalkan.",
-        confirmButtonText: "OK",
-        confirmButtonColor: "#3085d6",
-        showCloseButton: true,
-      });
-
+      successSwal("Order berhasil dibatalkan.");
       return response.data;
     } catch (error) {
-      await Swal.fire({
-        icon: "error",
-        title: "Gagal Membatalkan Order.",
-        text: error.response?.data?.errors || "Terjadi kesalahan, coba lagi.",
-        confirmButtonText: "Coba Lagi",
-        confirmButtonColor: "#d33",
-        showCloseButton: true,
-      });
+      errorSwal(error.response?.data?.errors);
     }
   },
   postReview: async (accessToken, orderId, rating, review) => {
@@ -126,25 +81,10 @@ const customerServices = {
         }
       );
 
-      await Swal.fire({
-        icon: "success",
-        title: "Berhasil",
-        text: "Ulasan Berhasil Di Kirim.",
-        confirmButtonText: "OK",
-        confirmButtonColor: "#3085d6",
-        showCloseButton: true,
-      });
-
+      successSwal("Ulasan berhasil dikirim");
       return response.data;
     } catch (error) {
-      await Swal.fire({
-        icon: "error",
-        title: "Gagal Mengirim Ulasan.",
-        text: error.response?.data?.errors || "Terjadi kesalahan, coba lagi.",
-        confirmButtonText: "Coba Lagi",
-        confirmButtonColor: "#d33",
-        showCloseButton: true,
-      });
+      errorSwal(error.response?.data?.errors);
     }
   },
   orderPayment: async (accessToken, orderId) => {
@@ -153,21 +93,12 @@ const customerServices = {
         headers: { Authorization: `Bearer ${accessToken}`, "Content-Type": "application/json" },
       });
 
-      console.log(response.data.data);
-
       const paymentUrl = response.data.data.url;
       window.location.href = paymentUrl;
 
       return response.data;
     } catch (error) {
-      await Swal.fire({
-        icon: "error",
-        title: "Gagal Mendapatkan link pembayaran.",
-        text: error.response?.data?.errors || "Terjadi kesalahan, coba lagi.",
-        confirmButtonText: "Coba Lagi",
-        confirmButtonColor: "#d33",
-        showCloseButton: true,
-      });
+      errorSwal(error.response?.data?.errors);
     }
   },
 };
