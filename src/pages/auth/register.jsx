707e4e62a7b24b70e78892184119acdf843683c8
unknown <anargya115@gmail.com>
2025-06-06 23:22:14 +0700

    update: redesign ui register

--- src/pages/auth/register.jsx
+++ src/pages/auth/register.jsx
@@ -111,30 +111,34 @@ const Register = () => {
 
   return (
     <div className="min-h-screen w-screen flex flex-row items-center justify-center">
-      <img src="/Images/woman.png" alt="" className="hidden h-screen lg:block lg:w-[40%] object-fit" />
-      <div className="register-form relative -screen w-screen flex flex-col items-center justify-center mt-[15px] mb-[20px] lg:mt-0 lg:mb-0 space-y-1 lg:w-[60%]">
+       <div className=" hidden h-screen bg-[#687eff] lg:block md:w-[50%] lg:relative lg:flex justify-end items-end rounded-tr-[40px] rounded-br-[40px]">
         <a href="/">
-          <img alt="backbiru" src="Images/backbiru.png" className="absolute top-8 left-5"></img>
+          <img alt="backwhite" src="/Images/backwhite.webp" className="absolute top-8 left-5"></img>
         </a>
-        <img src="Images/LogoAkucuciin.png" alt="logo" className="w-[200px] lg:w-[15rem]" />
-        <div className="flex flex-col items-center justify-center space-y-4">
-          <h1 className="font-bold text-[30px] font-poppins text-center">REGISTER</h1>
-          <h4 className="font-sans font-base font-[8px] text-gray55 text-center">Silahkan Isi Data Diri Anda</h4>
+        <div className="text-center text-white text-5xl font-bold font-['Montserrat'] whitespace-nowrap [text-shadow:_2px_2px_2px_rgb(0_0_0_/_0.25)] lg:absolute top-[15dvh] left-1/2 transform -translate-x-1/2">Welcome Back!</div>
+        <img src="/Images/mockup_hp.webp" alt="" className="hidden h-[80%] w-full lg:block object-cover" />
+      </div>
+      <div className="register-form relative -screen w-screen flex flex-col items-center justify-center mt-[15px] mb-[20px] lg:mt-0 lg:mb-0 space-y-1 lg:w-[50%]">
+        <a href="/">
+          <img alt="backbiru" src="Images/backbiru.png" className="absolute top-8 left-5 md:fixed  lg:hidden"></img>
+        </a>
+        <div className="flex flex-col items-center justify-center space-y-4 md:space-y-6">
+        <img src="Images/LogoAkucuciin.png" alt="logo" className="w-[200px] mt-0 md:w-[13rem] lg:w-[15rem] lg:hidden" />
 
+          <h1 className="font-normal font-[#423E3E] text-[20px] font-poppins text-center md:text-[32px]">Register new account</h1>
           <form onSubmit={handleSubmit} className="flex flex-col align-center justify-center items-center ">
-            <div className="flex flex-col align-center justify-center items-center lg:flex-row lg:pb-4 lg:space-x-3">
-              <div className="pb-4 space-y-4 flex flex-col align-center justify-center items-center lg:pb-0">
-                <div className="flex flex-row space-x-1 font-sans bg-white border border-0.2 border-gray-500/30 shadow-sm p-[10px] py-[10px] rounded-lg w-[20rem]">
+            <div className="flex flex-col align-center justify-center items-center md:space-y-5 lg:space-y-3">
+                <div className="space-x-1 flex flex-row font-sans bg-white border border-0.2 border-gray-500/30 shadow-sm p-[10px] py-[10px] rounded-lg w-[20rem] md:p-[12px] lg:p-[10px] md:w-[60dvw] lg:w-[30rem]">
                   <img src="Images/akunReg.png" alt="" className="w-[25px]" />
                   <input required type="text" name="name" value={formData.name} onChange={handleChange} placeholder="Nama" className="w-full font-sans bg-white text-gray-700 focus:shadow-none focus:outline-none focus:border-b-2" />
                 </div>
 
-                <div className="flex flex-row space-x-1 font-sans bg-white border border-0.2 border-gray-500/30 shadow-sm p-[10px] py-[10px] rounded-lg w-[20rem]">
-                  <img src="Images/emailReg2.png" alt="" className="w-[25px]" />
+                <div className="space-x-1 flex flex-row font-sans bg-white border border-0.2 border-gray-500/30 shadow-sm p-[10px] py-[10px] rounded-lg w-[20rem] md:p-[12px] lg:p-[10px] md:w-[60dvw] lg:w-[30rem]">
+                  <img src="Images/email.webp" alt="" className="w-[25px]" />
                   <input required type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Email" className="w-full font-sans bg-white text-gray-700 focus:shadow-none focus:outline-none focus:border-b-2" />
                 </div>
 
-                <div className="flex flex-row space-x-1 font-sans bg-white border border-0.2 border-gray-500/30 shadow-sm p-[10px] rounded-lg w-[20rem]">
+                <div className="space-x-1 flex flex-row font-sans bg-white border border-0.2 border-gray-500/30 shadow-sm p-[10px] rounded-lg w-[20rem] md:p-[12px] lg:p-[10px] md:w-[60dvw] lg:w-[30rem]">
                   <img src="Images/callReg.png" alt="" className="w-[25px]" />
                   <span className="text-gray-700 font-sans flex items-center">+</span>
                   <input
@@ -147,14 +151,12 @@ const Register = () => {
                     className="w-full font-sans bg-white text-gray-700 focus:shadow-none focus:outline-none focus:border-b-2"
                   />
                 </div>
-              </div>
-              <div className="pb-2 space-y-4 flex flex-col align-center justify-center items-center lg:pb-0">
-                <div className="flex flex-row space-x-1 font-sans bg-white border border-0.2 border-gray-500/30 shadow-sm p-[10px] py-[10px] rounded-lg w-[20rem]">
+                <div className="space-x-1 flex flex-row font-sans bg-white border border-0.2 border-gray-500/30 shadow-sm p-[10px] py-[10px] rounded-lg w-[20rem] md:p-[12px] lg:p-[10px] md:w-[60dvw] lg:w-[30rem]">
                   <img src="Images/address.png" alt="" className="w-[25px]" />
                   <input required type="text" name="address" value={formData.address} onChange={handleChange} placeholder="Alamat" className="w-full font-sans bg-white text-gray-700 focus:shadow-none focus:outline-none focus:border-b-2" />
                 </div>
-                <div className="flex flex-row justify-center align-center space-x-1 font-sans bg-white border border-0.2 border-gray-500/30 shadow-sm px-[10px] py-[10px] rounded-lg w-[20rem]">
-                  <img src="Images/passwordReg.png" alt="" className="w-[25px]" />
+                <div className="space-x-1 flex flex-row justify-center align-center font-sans bg-white border border-0.2 border-gray-500/30 shadow-sm px-[10px] py-[10px] rounded-lg w-[20rem] md:p-[12px] lg:p-[10px] md:w-[60dvw] lg:w-[30rem]">
+                  <img src="Images/password.webp" alt="" className="w-[25px]" />
                   <input
                     required
                     type={showPassword ? "text" : "password"}
@@ -165,11 +167,11 @@ const Register = () => {
                     className="w-full font-sans bg-white text-gray-700 focus:outline-none focus:border-b-2"
                   />
                   <button type="button" onClick={togglePasswordVisibility} className="flex justify-center align-center">
-                    {showPassword ? <img src="Images/invisible.png" className="w-[25px] h-[25px]" alt=""></img> : <img src="Images/visible.png" className="w-[25px] h-[25px]" alt=""></img>}
+                    {showPassword ? <img src="Images/Invisible.webp" className="w-[25px] h-[25px]" alt=""></img> : <img src="Images/visible.png" className="w-[25px] h-[25px]" alt=""></img>}
                   </button>
                 </div>
-                <div className="flex flex-row justify-center align-center space-x-1 font-sans bg-white border border-0.2 border-gray-500/30 shadow-sm px-[10px] py-[10px] rounded-lg w-[20rem]">
-                  <img src="Images/passwordReg.png" alt="" className="w-[25px]" />
+                <div className="space-x-1 flex flex-row justify-center align-center font-sans bg-white border border-0.2 border-gray-500/30 shadow-sm px-[10px] py-[10px] rounded-lg w-[20rem] md:p-[12px] lg:p-[10px] md:w-[60dvw] lg:w-[30rem]">
+                  <img src="Images/password.webp" alt="" className="w-[25px]" />
                   <input
                     required
                     type={showPassword2 ? "text" : "password"}
@@ -180,19 +182,18 @@ const Register = () => {
                     className="w-full font-sans bg-white text-gray-700  focus:outline-none focus:border-b-2"
                   />
                   <button type="button" onClick={togglePasswordVisibility2} className=" flex justify-center align-center">
-                    {showPassword2 ? <img src="Images/invisible.png" className="w-[25px] h-[25px]" alt=""></img> : <img src="Images/visible.png" className="w-[25px] h-[25px]" alt=""></img>}
+                    {showPassword2 ? <img src="Images/Invisible.webp" className="w-[25px] h-[25px]" alt=""></img> : <img src="Images/visible.png" className="w-[25px] h-[25px]" alt=""></img>}
                   </button>
                 </div>
               </div>
-            </div>
-            <p className="my-1 font-sans text-[#687eff] text-sm py-1">Notes: Password minimal 8 karakter</p>
+            <p className="my-1 font-sans text-[#687eff] text-sm py-1 md:text-base md:py-2">Notes: Password minimal 8 karakter</p>
 
-            {errorPassword && <p className="font-sans text-red-500 text-sm py-2">{errorPassword}</p>}
+            {errorPassword && <p className="font-sans text-red-500 text-sm py-2 md:text-base  md:py-2">{errorPassword}</p>}
 
             <button
               type="submit"
               disabled={isLoading}
-              className={`shadow-md font-sans w-[20rem] ${
+              className={`shadow-md font-sans w-[20rem] md:p-[12px] lg:p-[10px] md:w-[60dvw] lg:w-[30rem] md:text-base md:mt-4 ${
                 isLoading ? "bg-gray-400 text-gray-600 cursor-not-allowed" : "bg-[#687eff] text-white"
               } text-white font-semibold p-3 rounded-[10px] lg:p-4 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2`}
             >
@@ -200,17 +201,23 @@ const Register = () => {
             </button>
           </form>
 
-          <div className="border border-0.2 border-gray-500/30 shadow-sm rounded-[10px] space-y-5 w-[20rem] flex align-center items-center flex-col justify-center">
-            <button onClick={handleOauth} className="flex justify-center items-center font-sans w-[19rem] bg-white p-3 rounded-[10px] lg:p-4 focus:outline-none focus:ring-0.2 focus:ring-gray-500/30 focus:ring-offset-0.2">
+          <div className="flex justify-center items-center flex-row">
+            <div className="w-[10.5rem] border-t border-zinc-400"></div>
+            <div className="px-2 text-zinc-900/70 text-base font-normal font-['Montserrat'] whitespace-nowrap">Or Sign Up With</div>
+            <div className="w-[10.5rem] border-t border-zinc-400"></div>
+          </div>
+
+          <div className="border border-0.2 border-gray-500/30 shadow-sm rounded-[10px] space-y-5 w-[20rem] flex align-center items-center flex-col justify-center md:w-[60dvw] lg:w-[30rem]" >
+            <button onClick={handleOauth} className="flex justify-center items-center font-sans w-[19rem] bg-white p-3 rounded-[10px] md:w-[24rem] lg:p-4 focus:outline-none  focus:ring-0.2 focus:ring-gray-500/30 focus:ring-offset-0.2">
               <img src="Images/google.png" className="w-6 h-6" alt="Google Icon" />
-              <p className="ml-2 font-sans text-gray-500 text-center text-sm">Sign Up with Google</p>
+              <p className="ml-2 font-sans text-gray-500 text-center text-sm md:text-base">Sign Up with Google</p>
             </button>
           </div>
         </div>
-        <p className="font-sans text-gray-500 text-center text-sm">
+        <p className="font-sans text-gray-500 text-center text-sm md:text-base  md:py-2 md:mt-4">
           Sudah punya akun? {"  "}
           <Link to="/login">
-            <button className="font-sans bg-white text-[#687eff] font-normal ">Sign In</button>
+            <button className="font-sans bg-white text-[#687eff] font-normal md:text-base">Sign In</button>
           </Link>
         </p>
       </div>
