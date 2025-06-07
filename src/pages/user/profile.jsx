d78e03157cc302b256cb89219d8000b58a53403b
unknown <anargya115@gmail.com>
2025-06-07 07:21:41 +0700

    update: modified ui profile

--- src/pages/user/profile.jsx
+++ src/pages/user/profile.jsx
@@ -79,37 +79,42 @@ const Profile = () => {
 
   return (
     <div className="min-h-screen w-screen flex flex-row items-center justify-center">
-      <img src="/Images/woman.png" alt="" className="hidden h-screen lg:block lg:w-[40%] object-fit " />
-      <div className="h-screen p-6 bg-white rounded-lg md:justify-center md:mt-0 md:mx-0 md:w-[60%]">
+      <div className=" hidden h-screen bg-[#687eff] lg:block md:w-[50%] lg:relative lg:flex justify-end items-end rounded-tr-[40px] rounded-br-[40px]">
         <a href="/">
-          <img alt="backbiru" src="/Images/backbiru.png" className="fixed top-8 left-5 lg:relative "></img>
+          <img alt="backwhite" src="/Images/backwhite.webp" className="absolute top-8 left-5"></img>
+        </a>
+        <div className="text-center text-white text-5xl font-bold font-['Montserrat'] whitespace-nowrap [text-shadow:_2px_2px_2px_rgb(0_0_0_/_0.25)] lg:absolute top-[15dvh] left-1/2 transform -translate-x-1/2">Welcome Back!</div>
+        <img src="/Images/mockup_hp.webp" alt="" className="hidden h-[80%] w-full lg:block object-cover" />
+      </div>
+      <div className="h-full flex items-center flex-col p-6 bg-white rounded-lg md:justify-center md:mt-0 md:mx-0 md:w-[70%] lg:w-[50%]">
+        <a href="/">
+          <img alt="backbiru" src="/Images/backbiru.png" className="fixed top-8 left-5 lg:relative lg:hidden"></img>
         </a>
         <div className="flex justify-between flex-col space-y-6 items-center mb-6">
-          <h2 className="font-quick text-2xl font-semibold">PROFILE</h2>
-          <img alt="profile" src="Images/profile.png" className="w-[8em]"></img>
-          <button onClick={() => setIsEditing(true)} className="text-blue-500 bg-white  font-quick hover:underline">
-            Change profile
+          <h2 className="font-['Montserrat'] text-2xl font-semibold">PROFILE</h2>
+          <button onClick={() => setIsEditing(true)} className="text-blue-500 bg-white  font-['Montserrat'] hover:underline">
+            Edit Profile
           </button>
         </div>
 
         <form onSubmit={handleSubmit}>
           <div className="space-y-4">
             <div>
-              <label className="font-bold block font-quick text-sm text-gray-700">Name</label>
+              <label className="font-bold block font-['Montserrat'] text-sm text-gray-700">Name</label>
               {isEditing ? (
                 <input type="text" name="name" value={editProfile.name} onChange={handleChangeProfile} className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
               ) : (
-                <p className="mt-1 text-gray-900 font-quick">{profile.name}</p>
+                <p className="mt-1 text-gray-900 font-['Montserrat']">{profile.name}</p>
               )}
             </div>
 
             <div>
-              <label className="font-quick block text-sm font-bold text-gray-700">E-mail</label>
-              <p className="font-quick mt-1 text-gray-900">{profileData.data.email}</p>
+              <label className="font-['Montserrat'] block text-sm font-bold text-gray-700">E-mail</label>
+              <p className="font-['Montserrat'] mt-1 text-gray-900">{profileData.data.email}</p>
             </div>
 
             <div>
-              <label className="font-quick block text-sm font-bold text-gray-700">Phone</label>
+              <label className="font-['Montserrat'] block text-sm font-bold text-gray-700">Phone</label>
               {isEditing ? (
                 <input
                   type="text"
@@ -119,23 +124,23 @@ const Profile = () => {
                   className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
               ) : (
-                <p className="font-quick mt-1 text-gray-900">{profile.telephone}</p>
+                <p className="font-['Montserrat'] mt-1 text-gray-900">{profile.telephone}</p>
               )}
             </div>
 
             <div>
-              <label className="font-quick block text-sm font-bold text-gray-700">Address</label>
+              <label className="font-['Montserrat'] block text-sm font-bold text-gray-700">Address</label>
               {isEditing ? (
                 <input type="text" name="address" value={editProfile.address} onChange={handleChangeProfile} className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
               ) : (
-                <p className="font-quick mt-1 text-gray-900">{profile.address}</p>
+                <p className="font-['Montserrat'] mt-1 text-gray-900">{profile.address}</p>
               )}
             </div>
 
             <div>
-              <label className="font-quick block text-sm font-bold text-gray-700">Kode Referal</label>
+              <label className="font-['Montserrat'] block text-sm font-bold text-gray-700">Kode Referal</label>
               {profileData.data.referral_code ? (
-                <p className="font-quick mt-1 text-gray-900">{profileData.data.referral_code}</p>
+                <p className="font-['Montserrat'] mt-1 text-gray-900">{profileData.data.referral_code}</p>
               ) : inputReferral ? (
                 <div>
                   <button onClick={handleSubmitReferral} className="mt-2 px-4 py-2 bg-green-500 text-white font-semibold rounded-lg hover:bg-green-600 transition">
@@ -160,12 +165,12 @@ const Profile = () => {
             </div>
 
             <div>
-              <label className="font-quick block text-sm font-bold text-gray-700">Jumlah Pemakai Kode Referal : {profileData.data.referral_code_success_count} </label>
+              <label className="font-['Montserrat'] block text-sm font-bold text-gray-700">Jumlah Pemakai Kode Referal : {profileData.data.referral_code_success_count} </label>
             </div>
 
             {isEditing && (
               <div className="flex justify-end mt-4">
-                <button type="submit" className="font-quick px-6 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600">
+                <button type="submit" className="font-['Montserrat'] px-6 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600">
                   Save
                 </button>
               </div>
@@ -175,11 +180,10 @@ const Profile = () => {
         <div className="flex flex-col items center justify-center">
           <button
             onClick={handleLogout}
-            className="self-center mt-8 shadow-md font-sans w-[100px]  md:w-[130px] lg:w-[160px]  bg-red-500 text-white font-semibold px-1 py-2 rounded-[20px] focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+            className="self-end mt-8 shadow-md font-sans w-[100px]  md:w-[130px] lg:w-[160px]  bg-red-500 text-white font-semibold p-2 rounded-[8px] focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
           >
             Log Out
           </button>
-          <footer className="font-quick mt-[4em] text-center text-sm text-gray-500">HIGHLY PROFESSIONAL CLEANING</footer>
         </div>
       </div>
     </div>
