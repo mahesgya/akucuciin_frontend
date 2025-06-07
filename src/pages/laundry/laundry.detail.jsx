3d4f2b3ec28a4634589ae5648bbcddd14fc0d6ca
unknown <anargya115@gmail.com>
2025-06-07 08:13:19 +0700

    feat: add rating feature and top picks package

--- src/pages/laundry/laundry.detail.jsx
+++ src/pages/laundry/laundry.detail.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { BiCheck, BiTimeFive } from "react-icons/bi";
+import { BiCheck } from "react-icons/bi";
 import { AiFillStar } from "react-icons/ai";
 
 import FotoCarousel from "../../components/carousel/laundry.foto";
@@ -11,6 +11,8 @@ import { useSelector } from "react-redux";
 
 const LaundryDetail = () => {
   const [data, setData] = useState([]);
+  const [topPicks, setTopPicks] = useState([]);
+  const [rating, setRating] = useState([]);
   const [img, setImg] = useState([]);
   const { city, idlaundry } = useParams();
 
@@ -40,8 +42,13 @@ const LaundryDetail = () => {
     const fetchData = async () => {
       const response = await laundryServices.getById(idlaundry);
       const responseImg = await laundryServices.getImages(idlaundry);
+      const responseTop = await laundryServices.getTopPicksPackages(idlaundry);
+      const responseRating = await laundryServices.getLaundryRating(idlaundry);
+      console.log(responseRating);
       setData(response.data);
       setImg(responseImg.data);
+      setTopPicks(responseTop.data);
+      setRating(responseRating.data);
     };
 
     fetchData();
@@ -84,25 +91,84 @@ const LaundryDetail = () => {
               <div className="w-full rounded-lg max-w-screen-md">
                 <p className="font-['Montserrat'] text-gray-700 text-sm font-normal text-justify md:text-base leading-relaxed">{data.description}</p>
               </div>
+
+              {rating.avg_rating && (
+                <div className="absolute top-4 right-4 flex items-center bg-white px-2 py-1 rounded-full shadow">
+                  <AiFillStar className="text-[#F59E0B] mr-1 text-md lg:text-xl" />
+                  <span className="text-sm md:text-md lg:text-lg font-semibold text-[#F59E0B] font-['Montserrat']">{Number(rating.avg_rating).toFixed(1)}/5.0</span>
+                </div>
+              )}
             </div>
           </div>
 
           <div className="flex flex-col items-center justify-center gap-2 px-2 pb-4 w-full">
             <SearchBar value={searchQuery} onChangeText={setSearchQuery} onSubmit={handleSearch} />
 
+            <div className="relative mt-2">
+              <div className="mx-4 flex flex-row space-x-3 items-center md:mx-0">
+                <img src="/Images/toppicks.webp" className="w-[25px] h-[25px] md:w-[30px] md:h-[30px]" alt="thumbs up"></img>
+                <h4 class="justify-start text-neutral-700 text-md font-medium font-['Montserrat'] md:text-lg lg:text-xl">Laundry Top Picks</h4>
+              </div>
+              <div className="my-2 w-[100dvw] md:w-[95dvw] lg:w-[95dvw] border-t border-zinc-200 md:my-4"></div>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-5">
+              {topPicks
+                .slice()
+                .sort((a, b) => Number(a.price_text) - Number(b.price_text))
+                .map((item) => (
+                  <div key={item.id} onClick={() => handlePesan(item.id)} className="relative bg-white shadow-sm border border-gray-300/30 rounded-lg p-6 cursor-pointer w-full min-w-[95dvw] md:min-w-[100px] ">
+                    <div className="flex items-start justify-between">
+                      <div className="mr-4">
+                        <h3 className="font-['Montserrat'] text-sm font-semibold text-gray-800 md:text-base">{item.name}</h3>
+                        <p className="font-['Montserrat'] text-[10px] text-gray-500 mt-0.5 md:text-[12px] lg:text-[14px]">{item.description}</p>
+                      </div>
+
+                      <span className="font-['Montserrat'] text-md font-bold text-[#40A578] md:text-lg">Rp&nbsp;{Number(item.price_text).toLocaleString("id-ID")}</span>
+                    </div>
+
+                    <hr className="my-2 border-gray-200" />
+
+                    <ul className="space-y-1">
+                      {item.features.split(",").map((fitur, index) => (
+                        <li key={index} className="flex items-center font-['Montserrat'] text-[10px] text-gray-600 md:text-[12px] lg:text-[14px]">
+                          <BiCheck className="mr-2 text-[#40A578] text-lg" />
+                          {fitur.trim()}
+                        </li>
+                      ))}
+                    </ul>
+
+                    {item.avg_rating && (
+                      <div className="absolute right-4 bottom-2 flex items-center justify-end mt-2">
+                        <AiFillStar className="mr-1 text-[#F59E0B] text-lg lg:text-2xl" />
+                        <span className="font-['Montserrat'] text-[14px] font-semibold text-[#F59E0B] md:text-lg">{item.avg_rating}/5.00</span>
+                      </div>
+                    )}
+                  </div>
+                ))}
+            </div>
+
+            <div className="mt-2">
+              <div className="mx-4 flex flex-row space-x-3 items-center md:mx-0">
+                <img src="/Images/thumbup.webp" className="w-[25px] h-[25px] md:w-[30px] md:h-[30px]" alt="thumbs up"></img>
+                <h4 class="justify-start text-neutral-700 text-md font-medium font-['Montserrat'] md:text-lg lg:text-xl">All Packages</h4>
+              </div>
+              <div className="my-2 w-[100dvw] md:w-[95dvw] lg:w-[95dvw] border-t border-zinc-200 md:my-4"></div>
+            </div>
+
             {filteredPackages.length === 0 ? (
               <div className="text-center py-6 text-gray-500">
                 <p>Tidak ada paket yang sesuai dengan pencarian Anda</p>
               </div>
             ) : (
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-3">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 md:gap-4">
                 {filteredPackages
                   .slice()
                   .sort((a, b) => Number(a.price_text) - Number(b.price_text))
                   .map((item) => (
                     <div key={item.id} onClick={() => handlePesan(item.id)} className="bg-white shadow-sm border border-gray-300/30 rounded-lg p-6 cursor-pointer w-full min-w-[95dvw] md:min-w-[100px] ">
                       <div className="flex items-start justify-between">
-                        <div>
+                        <div className="mr-4">
                           <h3 className="font-['Montserrat'] text-sm font-semibold text-gray-800 md:text-base">{item.name}</h3>
                           <p className="font-['Montserrat'] text-[10px] text-gray-500 mt-0.5 md:text-[12px] lg:text-[14px]">{item.description}</p>
                         </div>
