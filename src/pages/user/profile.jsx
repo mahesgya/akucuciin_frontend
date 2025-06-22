ae2289762b112889175e24db5002e85712ccf38a
Daulyoi <derp.daffnet@gmail.com>
2025-06-22 14:21:18 +0700

    redesigned profile ui for desktop

--- src/pages/user/profile.jsx
+++ src/pages/user/profile.jsx
@@ -78,112 +78,252 @@ const Profile = () => {
   }
 
   return (
-    <div className="min-h-screen w-screen flex flex-row items-center justify-center">
-      <div className=" hidden h-screen bg-[#687eff] lg:block md:w-[50%] lg:relative lg:flex justify-end items-end rounded-tr-[40px] rounded-br-[40px]">
+    <div className="min-h-screen w-screen flex flex-row items-center justify-center bg-[#F4F5FF]">
+      <div className="hidden h-screen bg-[#687eff] md:w-[50%] lg:relative lg:flex justify-end items-end rounded-tr-[40px] rounded-br-[40px]">
         <a href="/">
-          <img alt="backwhite" src="/Images/backwhite.webp" className="absolute top-8 left-5"></img>
+          <img
+            alt="backwhite"
+            src="/Images/backwhite.webp"
+            className="absolute top-8 left-5"
+          ></img>
         </a>
-        <div className="text-center text-white text-5xl font-bold font-['Montserrat'] whitespace-nowrap [text-shadow:_2px_2px_2px_rgb(0_0_0_/_0.25)] lg:absolute top-[15dvh] left-1/2 transform -translate-x-1/2">Welcome Back!</div>
-        <img src="/Images/mockup_hp.webp" alt="" className="hidden h-[80%] w-full lg:block object-cover" />
+        <div className="text-center text-white text-5xl font-bold font-['Montserrat'] whitespace-nowrap [text-shadow:_2px_2px_2px_rgb(0_0_0_/_0.25)] lg:absolute top-[15dvh] left-1/2 transform -translate-x-1/2">
+          Welcome Back!
+        </div>
+        <img
+          src="/Images/mockup_hp.webp"
+          alt=""
+          className="hidden h-[80%] w-full lg:block object-cover"
+        />
       </div>
-      <div className="h-full flex items-center flex-col p-6 bg-white rounded-lg md:justify-center md:mt-0 md:mx-0 md:w-[70%] lg:w-[50%]">
+      <div className="h-full flex items-center flex-col p-6 rounded-lg md:justify-center md:mt-0 md:mx-0 md:w-[70%] lg:w-[50%]">
         <a href="/">
-          <img alt="backbiru" src="/Images/backbiru.png" className="fixed top-8 left-5 lg:relative lg:hidden"></img>
+          <img
+            alt="backbiru"
+            src="/Images/backbiru.png"
+            className="fixed top-8 left-5 lg:relative lg:hidden"
+          ></img>
         </a>
         <div className="flex justify-between flex-col space-y-6 items-center mb-6">
-          <h2 className="font-['Montserrat'] text-2xl font-semibold">PROFILE</h2>
-          <button onClick={() => setIsEditing(true)} className="text-blue-500 bg-white  font-['Montserrat'] hover:underline">
-            Edit Profile
-          </button>
+          <h2 className="font-['Montserrat'] text-2xl font-semibold">
+            {profile.name}
+          </h2>
         </div>
 
-        <form onSubmit={handleSubmit}>
-          <div className="space-y-4">
-            <div>
-              <label className="font-bold block font-['Montserrat'] text-sm text-gray-700">Name</label>
-              {isEditing ? (
-                <input type="text" name="name" value={editProfile.name} onChange={handleChangeProfile} className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
-              ) : (
-                <p className="mt-1 text-gray-900 font-['Montserrat']">{profile.name}</p>
+        <div className="bg-[#F4F5FF]">
+          <form
+            onSubmit={handleSubmit}
+            className="w-max max-w-lg bg-white rounded-lg shadow-md"
+          >
+            <div className="space-y-4 border border-gray-300 p-6 rounded-lg shadow-md">
+              <div className="flex gap-4 items-center border-b border-gray-300 pb-4">
+                <div className="flex items-center gap-4 w-full">
+                  <img
+                    src="/Images/email.webp"
+                    alt="email"
+                    className="w-10 h-10"
+                  />
+                  <div>
+                    <label className="font-['Montserrat'] block text-md font-bold text-gray-700">
+                      Email
+                    </label>
+                    <p className="font-['Montserrat'] mt-1 text-gray-900">
+                      {profileData.data.email}
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex gap-4 items-center border-b border-gray-300 pb-4">
+                <div className="flex items-center gap-4 w-full">
+                  <img
+                    src="/Images/telephone.webp"
+                    alt="telephone"
+                    className="w-10 h-10"
+                  />
+                  <div>
+                    <label className="font-['Montserrat'] block text-md font-bold text-gray-700">
+                      Phone
+                    </label>
+                    {isEditing ? (
+                      <input
+                        type="text"
+                        name="telephone"
+                        value={editProfile.telephone}
+                        onChange={handleChangeProfile}
+                        className="mt-1 w-[400px] px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                      />
+                    ) : (
+                      <p className="font-['Montserrat'] mt-1 text-gray-900">
+                        {profile.telephone}
+                      </p>
+                    )}
+                  </div>
+                </div>
+                {!isEditing && (
+                  <button
+                    onClick={() => setIsEditing(true)}
+                    className="bg-transparent"
+                    type="button"
+                  >
+                    <img
+                      src="/Images/pencilbiru.webp"
+                      alt="edit"
+                      className="w-8 cursor-pointer"
+                    />
+                  </button>
+                )}
+              </div>
+
+              <div className="flex gap-4 items-center">
+                <div className="flex items-center gap-4 w-full">
+                  <img
+                    src="/Images/address.webp"
+                    alt="address"
+                    className="w-10"
+                  />
+                  <div>
+                    <label className="font-['Montserrat'] block text-md font-bold text-gray-700">
+                      Address
+                    </label>
+                    {isEditing ? (
+                      <input
+                        type="text"
+                        name="address"
+                        value={editProfile.address}
+                        onChange={handleChangeProfile}
+                        className="mt-1 w-[400px] px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                      />
+                    ) : (
+                      <p className="font-['Montserrat'] mt-1 text-gray-900">
+                        {profile.address}
+                      </p>
+                    )}
+                  </div>
+                </div>
+
+                {!isEditing && (
+                  <button
+                    onClick={() => setIsEditing(true)}
+                    className="bg-transparent"
+                    type="button"
+                  >
+                    <img
+                      src="/Images/pencilbiru.webp"
+                      alt="edit"
+                      className="w-8 cursor-pointer"
+                    />
+                  </button>
+                )}
+              </div>
+
+              {isEditing && (
+                <div className="flex justify-end mt-4">
+                  <button
+                    type="submit"
+                    className="font-['Montserrat'] px-6 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600"
+                  >
+                    Save
+                  </button>
+                </div>
               )}
             </div>
+          </form>
 
-            <div>
-              <label className="font-['Montserrat'] block text-sm font-bold text-gray-700">E-mail</label>
-              <p className="font-['Montserrat'] mt-1 text-gray-900">{profileData.data.email}</p>
-            </div>
+          {}
 
+          <div className="mt-6 space-y-4 w-full">
             <div>
-              <label className="font-['Montserrat'] block text-sm font-bold text-gray-700">Phone</label>
-              {isEditing ? (
-                <input
-                  type="text"
-                  name="telephone"
-                  value={editProfile.telephone}
-                  onChange={handleChangeProfile}
-                  className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-              ) : (
-                <p className="font-['Montserrat'] mt-1 text-gray-900">{profile.telephone}</p>
-              )}
-            </div>
-
-            <div>
-              <label className="font-['Montserrat'] block text-sm font-bold text-gray-700">Address</label>
-              {isEditing ? (
-                <input type="text" name="address" value={editProfile.address} onChange={handleChangeProfile} className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500" />
-              ) : (
-                <p className="font-['Montserrat'] mt-1 text-gray-900">{profile.address}</p>
-              )}
-            </div>
-
-            <div>
-              <label className="font-['Montserrat'] block text-sm font-bold text-gray-700">Kode Referal</label>
               {profileData.data.referral_code ? (
-                <p className="font-['Montserrat'] mt-1 text-gray-900">{profileData.data.referral_code}</p>
+                <div className="flex flex-col bg-white p-4 space-y-4 rounded-lg shadow-md">
+                  <div className="flex  w-full items-center border-b border-gray-300 pb-4">
+                    <img
+                      src="/Images/kodeReferralTag.webp"
+                      alt="kode referral"
+                      className="w-10 h-10 mr-4"
+                    />
+                    <div>
+                      <p className="font-['Montserrat'] block text-md font-bold text-gray-700">
+                        Kode Referral
+                      </p>
+                      <p className="font-['Montserrat'] mt-1 text-gray-900">
+                        {profileData.data.referral_code}
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="flex items-center w-full">
+                    <div className="flex items-center gap-4 w-full">
+                      <img
+                        src="/Images/kodeReferralGroup.webp"
+                        alt="pemakai kode referral"
+                        className="w-10 h-10"
+                      />
+                      <div>
+                        <p className="font-['Montserrat'] block text-sm font-bold text-gray-700">
+                          Jumlah Pemakai Kode Referral{" "}
+                        </p>
+                        <p>{profileData.data.referral_code_success_count}</p>
+                      </div>
+                    </div>
+                  </div>
+                  <p className="text-blue-500">Ajak {profileData.data.referral_code_until_next_reward} orang lagi untuk mendapatkan voucher</p>
+                </div>
               ) : inputReferral ? (
-                <div>
-                  <button onClick={handleSubmitReferral} className="mt-2 px-4 py-2 bg-green-500 text-white font-semibold rounded-lg hover:bg-green-600 transition">
-                    Kirim
-                  </button>
-                  <button onClick={handleInputReferral} className="mt-2 ml-3  px-4 py-2 bg-red-500 text-white font-semibold rounded-lg hover:bg-red-600 transition">
-                    Batal
-                  </button>
+                <div className="flex flex-col items-center bg-white p-4 space-y-4 rounded-lg shadow-md">
+                  <p className="font-['Montserrat'] block text-lg pb-4 font-bold text-gray-700">
+                    Buat Kode Referral
+                  </p>
                   <input
                     type="text"
                     name="referral_code"
+                    placeholder="Masukkan Kode Referral"
                     value={referralCode.referral_code}
                     onChange={handleChangeReferral}
-                    className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    className="mt-1 block w-[80%] px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                   ></input>
+
+                  <p className="text-blue-500">
+                    Pastikan sudah benar karena tidak bisa diganti lagi
+                  </p>
+
+                  <div className="flex justify-around w-[80%]">
+                    <button
+                      onClick={handleSubmitReferral}
+                      className="w-[40%] mt-2 px-4 py-2 bg-blue-500 text-white border font-semibold rounded-lg hover:bg-blue-600 transition"
+                    >
+                      Kirim
+                    </button>
+
+                    <div className="w-[40%] mt-2 ml-3 px-4 py-2 border border-blue-500 rounded-lg">
+                      <button
+                        onClick={handleInputReferral}
+                        className="w-full text-blue-500 bg-white font-semibold rounded-lg transition"
+                      >
+                        Batal
+                      </button>
+                    </div>
+                  </div>
                 </div>
               ) : (
-                <button onClick={handleInputReferral} className="mt-2 px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 transition">
-                  Tambah Kode Referral
+                <button
+                  onClick={handleInputReferral}
+                  className="mt-2 px-4 py-2 w-full bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 transition"
+                >
+                  Bikin Kode Referral
                 </button>
               )}
             </div>
-
-            <div>
-              <label className="font-['Montserrat'] block text-sm font-bold text-gray-700">Jumlah Pemakai Kode Referal : {profileData.data.referral_code_success_count} </label>
-            </div>
-
-            {isEditing && (
-              <div className="flex justify-end mt-4">
-                <button type="submit" className="font-['Montserrat'] px-6 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600">
-                  Save
-                </button>
-              </div>
-            )}
           </div>
-        </form>
-        <div className="flex flex-col items center justify-center">
-          <button
-            onClick={handleLogout}
-            className="self-end mt-8 shadow-md font-sans w-[100px]  md:w-[130px] lg:w-[160px]  bg-red-500 text-white font-semibold p-2 rounded-[8px] focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
-          >
-            Log Out
-          </button>
+
+          <div className="flex flex-col items center justify-center w-full">
+            <button
+              onClick={handleLogout}
+              className="mt-8 py-3 shadow-md font-sans w-full bg-red-500 hover:bg-red-600 text-white font-semibold p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+            >
+              Log Out
+            </button>
+          </div>
         </div>
       </div>
     </div>
