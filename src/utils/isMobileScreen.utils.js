cb3dad9ada0f4565f57d8ef11aaad105f908e0bb
Daulyoi <derp.daffnet@gmail.com>
2025-07-15 12:58:59 +0700

    feat: add utility function to determine if the device is mobile or desktop

---
+++ src/utils/isMobileScreen.utils.js
@@ -0,0 +1,16 @@
+// useIsMobile.js
+import { useState, useEffect } from "react";
+
+const useIsMobileScreen = (breakpoint = 768) => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < breakpoint);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < breakpoint);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, [breakpoint]);
+
+  return isMobile;
+};
+
+export default useIsMobileScreen;
