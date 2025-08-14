9f92c2cebc94249941504f30b1c2da31e8db5614
mahesgya <anargya115@gmail.com>
2025-08-15 00:43:26 +0800

    feat: make interceptors commit for 401 errors

--- src/index.js
+++ src/index.js
@@ -4,12 +4,76 @@ import Routess from "./routes/routes";
 import ScrollToTop from "./hooks/scroll.top";
 
 import { BrowserRouter, useRoutes } from "react-router-dom";
-import { store, persistor } from "./redux/store"; 
+import { store, persistor } from "./redux/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { register as registerServiceWorker } from './serviceWorkerRegistration';
+import { register as registerServiceWorker } from "./serviceWorkerRegistration";
+
+import axios from "axios";
+import Cookies from "js-cookie";
+import { setLogin, setLogout } from "./redux/auth.slicer";
 
 registerServiceWorker();
+
+const BASE_URL = process.env.REACT_APP_BASE_BACKEND_URL;
+let refreshPromise = null; 
+
+async function refreshToken() {
+  if (!refreshPromise) {
+    refreshPromise = (async () => {
+      const oldRefreshToken = Cookies.get("refreshToken");
+      if (!oldRefreshToken) throw new Error("No refresh token");
+
+      const res = await axios.put(`${BASE_URL}/api/auth`, { refresh_token: oldRefreshToken });
+
+      const { accessToken, refreshToken } = res.data.data;
+      Cookies.set("accessToken", accessToken, { secure: true, sameSite: "none", expires: 1 });
+      Cookies.set("refreshToken", refreshToken, { secure: true, sameSite: "none", expires: 7 });
+
+      await store.dispatch(setLogin({ accessToken, refreshToken }));
+      return accessToken;
+    })()
+    .finally(() => {
+      refreshPromise = null;
+    });
+  }
+  return refreshPromise;
+}
+
+axios.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const original = error.config || {};
+
+    if (error.response?.status !== 401) return Promise.reject(error);
+    const isRefreshCall = original.url?.includes("/api/auth");
+
+    if (isRefreshCall) {
+      await store.dispatch(setLogout());
+      Cookies.remove("accessToken");
+      Cookies.remove("refreshToken");
+      return Promise.reject(error);
+    }
+
+    if (original._retry) {
+      return Promise.reject(error);
+    }
+    original._retry = true;
+
+    try {
+      const newAccess = await refreshToken();
+
+      original.headers = {
+        ...original.headers,
+        Authorization: `Bearer ${newAccess}`,
+      };
+
+      return axios.request(original);
+    } catch (e) {
+      return Promise.reject(e);
+    }
+  }
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
