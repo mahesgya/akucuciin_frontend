c29a20e9eb5dbbc763ac9bd04d2f59f386878da3
Daulyoi <derp.daffnet@gmail.com>
2025-09-20 20:19:08 +0700

    changed promos that are shown

--- src/data/promo.images.js
+++ src/data/promo.images.js
@@ -1,10 +1,8 @@
 const PromoImages = [
-  { id: 1, filename: "promo1.webp" },
   { id: 2, filename: "promo2.webp" },
 ];
 
 const PromoImagesDesktop = [
-  { id: 1, filename: "promo1_desktop.webp" },
   { id: 2, filename: "promo2_desktop.webp" },
 ];
 
